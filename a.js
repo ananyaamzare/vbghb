7180a821c411d8e30aa5d7a719058b70f92c9a31
ananya <ananyaamzare30@gmail.com>
2024-08-26 15:50:23 +0530

    ann

---
+++ a.js
@@ -0,0 +1,73 @@
+// console.log("hello!");
+
+// "abc".toUpperCase()
+
+// [1, 2, 3].push(4);
+// let a=20;
+// let b=30;
+// console.log(a+b)
+
+// function mul (a,b,c) {
+//     console.log(a*b*c);
+// }
+
+// mul (3,4,5)
+
+// function fact (a) {
+//     p=1
+//     for (let i=1; i <= a; i++){
+//         p=p*i
+//     }
+//     console.log(p)
+     
+// }
+
+// fact (6)
+
+// function Prime (a) {
+    
+
+//     if (a <= 1) {
+//         console.log("no")
+
+//     }
+//     for (let i=2; i < a; i++)
+
+//     if (a % 2 === 0 ){
+//         console.log("no")
+//     }
+//     console.log("yes")
+// }
+// prime (5)
+
+// function prime(a) {
+//     if (a <= 1) {
+//         console.log("no");
+//         return;
+//     }
+
+//     for (let i = 2; i < a; i++) {
+//         if (a % i === 0) {
+//             console.log("no");
+//             return;
+//         }
+//     }
+//     console.log("yes");
+// }
+
+// prime(11);
+ 
+
+function evenArray (a, b, c, d) {
+
+    for (let i=0; i<= 4; i++)
+
+    if ( [i] % 2 === 0 ) {
+
+
+    }
+
+    console.log( i + "is an even number")
+}
+
+evenArray (1,2,3,4); 
